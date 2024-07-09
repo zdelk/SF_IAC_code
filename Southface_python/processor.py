c75a009706bc93b60668364f1743809fdf28b179
Zachary Delk <randomaccburner@gmail.com>
2024-07-09 13:07:27 -0400

    Script should be final executable in the end

---
+++ Southface_python/processor.py
@@ -0,0 +1,44 @@
+# Plan: Simple script that will take in a workbook,
+# preform the analysis, and then output to another workbook
+import pandas as pd
+from utilities_analysis import utility_analysis
+from KSU_IAC_Functions import pipe_final
+
+
+input_path = 'test_input.xlsx'
+output_path = 'test_output.xlsx'
+
+# Input should just be the path of the orginal workbook,
+# and path of the output
+
+def IAC_function(input_path, output_path):
+    input_workbook = pd.read_excel(input_path, engine = 'openpyxl', sheet_name = None)
+    
+    utility_bill = input_workbook['Utility Bills']
+    per_kwh_cost, per_kw_peak_cost = utility_analysis(utility_bill)
+    
+    bill_analysis = pd.DataFrame({
+        'per_kwh_cost': [per_kwh_cost],
+        'per_kw_peak_cost': [per_kw_peak_cost]
+    })
+    
+    pipe_data = input_workbook['Pipe Data']
+    pipe_cost_data, pipe_table_data, pipe_heat_savings = pipe_final(pipe_data, per_kw_peak_cost, per_kwh_cost, 
+                     per_MMBTU_cost=None, fuel_source='Electric')
+    
+    len1 = 1
+    len2 = len1 + len(pipe_cost_data) + 2
+    len3 = len2 + len(pipe_table_data) + 2
+
+    writer = pd.ExcelWriter(output_path, engine = 'xlsxwriter')
+
+    bill_analysis.to_excel(writer, sheet_name='Bill Analysis', index=False)
+        
+    pipe_cost_data.to_excel(writer, sheet_name='Pipe Data', index=False, startcol=1, startrow=len1)
+    pipe_table_data.to_excel(writer, sheet_name= 'Pipe Data', index=False, startcol=1, startrow=len2)
+    pipe_heat_savings.to_excel(writer, sheet_name = 'Pipe Data', index=False, startcol=1, startrow=len3)
+
+    writer.close()
+
+
+IAC_function(input_path, output_path)
