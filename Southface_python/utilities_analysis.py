16f3f6df1a5e0e002ac9e0197b03a72a8a9f4745
Zachary Delk <randomaccburner@gmail.com>
2024-07-09 15:52:09 -0400

    Added gas part

--- Southface_python/utilities_analysis.py
+++ Southface_python/utilities_analysis.py
@@ -1,21 +1,23 @@
 # Bill Analysis
 import pandas as pd
 
-utility_bill = pd.read_csv("bill_data(polycom).csv")
+#test_bill = pd.read_excel('bill_data(polycom).xlsx')
 
 def utility_analysis(utility_bill):
-    annual_bill = utility_bill.iloc[:,1:].sum(axis=0)
+    annual_bill = utility_bill.iloc[:,1:].sum(axis=0) # columns averages
     
-    per_kwh_cost = annual_bill.loc['kwh_charge'] / annual_bill.loc['kwh_usage']
-    per_kw_peak_cost = annual_bill.loc['peak_charge'] / annual_bill.loc['peak_usage']
-    return per_kwh_cost, per_kw_peak_cost
-
+    per_kwh_cost = annual_bill.loc['Kwh Charge ($)'] / annual_bill.loc['Kwh Usage']
+    per_kw_peak_cost = annual_bill.loc['Peak Charge ($)'] / annual_bill.loc['Peak Kw Usage']
     
+    per_therm_cost = None # Initialize as null value
+    if 'Natural Gas Usage (Therms)' in utility_bill.columns: #if gas is used, updates value
+        per_therm_cost = annual_bill.loc['therm_charge'] / annual_bill.loc['therm_charge']
     
+    return annual_bill, per_kwh_cost, per_kw_peak_cost, per_therm_cost
 
 
 
-
+#print(utility_analysis(test_bill))
 # # print(kwh_rate)
 # # print(peak_rate)
 
